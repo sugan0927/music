32ab9f1a9962d26c726f95bec173cbb3348988b9
gokadzev <gokadze.v1@gmail.com>
2022-07-31 11:31:34 +0400

    added slider in player (needs to increase width)

--- lib/ui/rootPage.dart
+++ lib/ui/rootPage.dart
@@ -26,6 +26,13 @@ class AppState extends State<Musify> {
   void initState() {
     super.initState();
     initAudioPlayer();
+    positionSubscription = audioPlayer?.positionStream
+        .listen((p) => {if (mounted) setState(() => position = p)});
+    audioPlayer?.durationStream.listen(
+      (d) => {
+        if (mounted) {setState(() => duration = d)}
+      },
+    );
   }
 
   @override
@@ -141,190 +148,241 @@ class AppState extends State<Musify> {
           child: Padding(
             padding: const EdgeInsets.only(top: 5, bottom: 2),
             child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 7,
-                    bottom: 7,
-                    right: 15,
-                    left: 15,
-                  ),
-                  child: ValueListenableBuilder<String>(
-                    valueListenable: highResImage,
-                    builder: (_, value, __) {
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: CachedNetworkImage(
-                          imageUrl: value,
-                          fit: BoxFit.fill,
-                          errorWidget: (context, url, error) => Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              gradient: const LinearGradient(
-                                colors: [
-                                  Color.fromARGB(30, 255, 255, 255),
-                                  Color.fromARGB(30, 233, 233, 233),
-                                ],
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 7,
+                        bottom: 7,
+                        right: 15,
+                        left: 15,
+                      ),
+                      child: ValueListenableBuilder<String>(
+                        valueListenable: highResImage,
+                        builder: (_, value, __) {
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: CachedNetworkImage(
+                              imageUrl: value,
+                              fit: BoxFit.fill,
+                              errorWidget: (context, url, error) => Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  gradient: const LinearGradient(
+                                    colors: [
+                                      Color.fromARGB(30, 255, 255, 255),
+                                      Color.fromARGB(30, 233, 233, 233),
+                                    ],
+                                  ),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      MdiIcons.musicNoteOutline,
+                                      size: 30,
+                                      color: accent,
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(
-                                  MdiIcons.musicNoteOutline,
-                                  size: 30,
+                          );
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.zero,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          ValueListenableBuilder<String>(
+                            valueListenable: title,
+                            builder: (_, value, __) {
+                              return Text(
+                                value.length > 15
+                                    ? '${title.value.substring(0, 15)}...'
+                                    : title.value,
+                                style: TextStyle(
                                   color: accent,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600,
                                 ),
-                              ],
-                            ),
+                              );
+                            },
                           ),
-                        ),
-                      );
-                    },
-                  ),
+                          ValueListenableBuilder<String>(
+                            valueListenable: artist,
+                            builder: (_, value, __) {
+                              return Text(
+                                value.length > 15
+                                    ? '${artist.value.substring(0, 15)}...'
+                                    : artist.value,
+                                style: TextStyle(
+                                  color: accent,
+                                  fontSize: 15,
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: EdgeInsets.zero,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      ValueListenableBuilder<String>(
-                        valueListenable: title,
-                        builder: (_, value, __) {
-                          return Text(
-                            value.length > 15
-                                ? '${title.value.substring(0, 15)}...'
-                                : title.value,
-                            style: TextStyle(
-                              color: accent,
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
-                            ),
+                Row(
+                  children: [
+                    if (position != null)
+                      Text(
+                        position != null
+                            ? '$positionText '.replaceFirst('0:0', '0')
+                            : duration != null
+                                ? durationText
+                                : '',
+                        style:
+                            const TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    if (duration != null)
+                      Slider(
+                        activeColor: accent,
+                        inactiveColor: Colors.green[50],
+                        value: position?.inMilliseconds.toDouble() ?? 0.0,
+                        onChanged: (double? value) {
+                          setState(() {
+                            audioPlayer!.seek(
+                              Duration(
+                                seconds: (value! / 1000).round(),
+                              ),
+                            );
+                            value = value;
+                          });
+                        },
+                        max: duration!.inMilliseconds.toDouble(),
+                      ),
+                    if (position != null)
+                      Text(
+                        position != null
+                            ? durationText.replaceAll('0:', '')
+                            : duration != null
+                                ? durationText
+                                : '',
+                        style:
+                            const TextStyle(fontSize: 18, color: Colors.white),
+                      )
+                  ],
+                ),
+                Row(
+                  children: [
+                    ValueListenableBuilder<bool>(
+                      valueListenable: songLikeStatus,
+                      builder: (_, value, __) {
+                        if (value == true) {
+                          return IconButton(
+                            color: accent,
+                            icon: const Icon(MdiIcons.star),
+                            onPressed: () => {
+                              removeUserLikedSong(ytid),
+                              songLikeStatus.value = false
+                            },
                           );
-                        },
-                      ),
-                      ValueListenableBuilder<String>(
-                        valueListenable: artist,
-                        builder: (_, value, __) {
-                          return Text(
-                            value.length > 15
-                                ? '${artist.value.substring(0, 15)}...'
-                                : artist.value,
-                            style: TextStyle(
-                              color: accent,
-                              fontSize: 15,
-                            ),
+                        } else {
+                          return IconButton(
+                            color: Colors.white,
+                            icon: const Icon(MdiIcons.starOutline),
+                            onPressed: () => {
+                              addUserLikedSong(ytid),
+                              songLikeStatus.value = true
+                            },
                           );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                ValueListenableBuilder<bool>(
-                  valueListenable: songLikeStatus,
-                  builder: (_, value, __) {
-                    if (value == true) {
-                      return IconButton(
-                        color: accent,
-                        icon: const Icon(MdiIcons.star),
-                        onPressed: () => {
-                          removeUserLikedSong(ytid),
-                          songLikeStatus.value = false
-                        },
-                      );
-                    } else {
-                      return IconButton(
-                        color: Colors.white,
-                        icon: const Icon(MdiIcons.starOutline),
-                        onPressed: () => {
-                          addUserLikedSong(ytid),
-                          songLikeStatus.value = true
-                        },
-                      );
-                    }
-                  },
-                ),
-                ValueListenableBuilder<bool>(
-                  valueListenable: shuffleNotifier,
-                  builder: (_, value, __) {
-                    return IconButton(
-                      padding: EdgeInsets.zero,
-                      icon: Icon(
-                        MdiIcons.shuffle,
-                        color: value ? accent : Colors.white,
-                      ),
-                      onPressed: changeShuffleStatus,
-                    );
-                  },
-                ),
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: Icon(
-                    Icons.skip_previous,
-                    color: hasPrevious ? Colors.white : Colors.grey,
-                  ),
-                  onPressed: playPrevious,
-                ),
-                ValueListenableBuilder<MPlayerState>(
-                  valueListenable: buttonNotifier,
-                  builder: (_, value, __) {
-                    return IconButton(
-                      icon: buttonNotifier.value == MPlayerState.playing
-                          ? const Icon(MdiIcons.pause)
-                          : const Icon(MdiIcons.playOutline),
-                      color: accent,
-                      splashColor: Colors.transparent,
-                      onPressed: () {
-                        setState(() {
-                          if (buttonNotifier.value == MPlayerState.playing) {
-                            audioPlayer?.pause();
-                          } else if (buttonNotifier.value ==
-                              MPlayerState.paused) {
-                            audioPlayer?.play();
-                          }
-                        });
+                        }
                       },
-                      iconSize: 45,
-                    );
-                  },
-                ),
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: Icon(
-                    Icons.skip_next,
-                    color: hasNext ? Colors.white : Colors.grey,
-                  ),
-                  onPressed: playNext,
-                ),
-                ValueListenableBuilder<bool>(
-                    valueListenable: repeatNotifier,
-                    builder: (_, value, __) {
-                      return IconButton(
-                        padding: EdgeInsets.zero,
-                        icon: Icon(
-                          MdiIcons.repeat,
-                          color: value ? accent : Colors.white,
-                        ),
-                        onPressed: changeLoopStatus,
-                      );
-                    }),
-                ValueListenableBuilder<bool>(
-                  valueListenable: playNextSongAutomatically,
-                  builder: (_, value, __) {
-                    return IconButton(
+                    ),
+                    ValueListenableBuilder<bool>(
+                      valueListenable: shuffleNotifier,
+                      builder: (_, value, __) {
+                        return IconButton(
+                          padding: EdgeInsets.zero,
+                          icon: Icon(
+                            MdiIcons.shuffle,
+                            color: value ? accent : Colors.white,
+                          ),
+                          onPressed: changeShuffleStatus,
+                        );
+                      },
+                    ),
+                    IconButton(
                       padding: EdgeInsets.zero,
                       icon: Icon(
-                        MdiIcons.chevronRight,
-                        color: value ? accent : Colors.white,
+                        Icons.skip_previous,
+                        color: hasPrevious ? Colors.white : Colors.grey,
                       ),
-                      onPressed: changeAutoPlayNextStatus,
-                    );
-                  },
-                ),
+                      onPressed: playPrevious,
+                    ),
+                    ValueListenableBuilder<MPlayerState>(
+                      valueListenable: buttonNotifier,
+                      builder: (_, value, __) {
+                        return IconButton(
+                          icon: buttonNotifier.value == MPlayerState.playing
+                              ? const Icon(MdiIcons.pause)
+                              : const Icon(MdiIcons.playOutline),
+                          color: accent,
+                          splashColor: Colors.transparent,
+                          onPressed: () {
+                            setState(() {
+                              if (buttonNotifier.value ==
+                                  MPlayerState.playing) {
+                                audioPlayer?.pause();
+                              } else if (buttonNotifier.value ==
+                                  MPlayerState.paused) {
+                                audioPlayer?.play();
+                              }
+                            });
+                          },
+                          iconSize: 45,
+                        );
+                      },
+                    ),
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(
+                        Icons.skip_next,
+                        color: hasNext ? Colors.white : Colors.grey,
+                      ),
+                      onPressed: playNext,
+                    ),
+                    ValueListenableBuilder<bool>(
+                        valueListenable: repeatNotifier,
+                        builder: (_, value, __) {
+                          return IconButton(
+                            padding: EdgeInsets.zero,
+                            icon: Icon(
+                              MdiIcons.repeat,
+                              color: value ? accent : Colors.white,
+                            ),
+                            onPressed: changeLoopStatus,
+                          );
+                        }),
+                    ValueListenableBuilder<bool>(
+                      valueListenable: playNextSongAutomatically,
+                      builder: (_, value, __) {
+                        return IconButton(
+                          padding: EdgeInsets.zero,
+                          icon: Icon(
+                            MdiIcons.chevronRight,
+                            color: value ? accent : Colors.white,
+                          ),
+                          onPressed: changeAutoPlayNextStatus,
+                        );
+                      },
+                    ),
+                  ],
+                )
               ],
             ),
           ),
